23db6e3adbaf64f01732fb8e47f5f2f1d9316435
JYR_RC <119703887+JYRRC@users.noreply.github.com>
2023-09-23 17:36:55 +0200

    Marble: BringUp Welcome Crdroid

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -8,9 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# MindGapps Core
-#$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
-
 # Inherit some common AlphaDroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
@@ -21,31 +18,11 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 # Source Flags
 TARGET_HAS_UDFPS := false
 TARGET_ENABLE_BLUR := true
-TARGET_EXCLUDES_AUDIOFX := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# GAPPS
-WITH_GAPPS := false
-TARGET_INCLUDE_GOOGLE_APP := false
-TARGET_INCLUDE_GOOGLE_CALCULATOR := true
-TARGET_INCLUDE_GOOGLE_CALENDAR := false
-TARGET_INCLUDE_GOOGLE_CHROME := false
-TARGET_INCLUDE_GOOGLE_DESKCLOCK := false
-TARGET_INCLUDE_GOOGLE_DRIVE := false
-TARGET_INCLUDE_GMAIL := false
-TARGET_INCLUDE_GOOGLE_MAPS := false
-TARGET_INCLUDE_GOOGLE_PHOTOS := true
-TARGET_INCLUDE_GOOGLE_SETUP := true
-
-
-
 # Adb
 TARGET_DEFAULT_ADB_ENABLED := true
-
-# Maintainer
-ALPHA_BUILD_TYPE := Official
-ALPHA_MAINTAINER := JYR_RC
 
 # Device identifier
 PRODUCT_DEVICE := marble
