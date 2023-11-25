14b2c765985a5fdbfc1d4e33c76f4590d15d4f7e
JYRRC <javixhtsweet@gmail.com>
2023-11-25 22:30:34 +0100

    marble: bringup cherishOS
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- cherish_marble.mk
+++ cherish_marble.mk
@@ -9,32 +9,40 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CherishOS stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# CherishOS stuff.
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-
-# CherishOS Build.
-CHERISH_BUILD_TYPE=OFFICIAL
-WITH_GMS := true
-#CHERISH_VANILLA := true
-#TARGET_USES_PICO_GAPPS := true
-#USE_PIXEL_CHARGING := true
-#TARGET_INCLUDE_CARRIER_SETTINGS := true
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=JYR_RC
-
-# Device identifier
+## Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := cherish_marble
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F5
+
+# Cherish Stuffs
+USE_LEGACY_BOOTANIMATION := true
+CHERISH_BUILD_TYPE := Official
+
+# Cherish props
+CHERISH_MAINTAINER := JYR_RC
+CHERISH_CHIPSET := SM7475
+CHERISH_BATTERY := 5000mAh
+CHERISH_DISPLAY := 1080x2400
+
+# Gapps
+WITH_GMS := true
+#CHERISH_VANILLA := true
+TARGET_USES_MINI_GAPPS := true
+#TARGET_USES_PICO_GAPPS := true
+#USE_PIXEL_CHARGING := true
+#TARGET_INCLUDE_CARRIER_SETTINGS := true
+
+# Nuke AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
