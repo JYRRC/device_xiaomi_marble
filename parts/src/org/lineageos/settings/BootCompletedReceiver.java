496aee419847fdb193c1ae361ec0eb422ac4acd1
JYRRC <javixhtsweet@gmail.com>
2023-12-06 18:53:18 +0100

    marble: re-enable dirac and app refresh rate
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -13,6 +13,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import org.lineageos.settings.camera.NfcCameraService;
+import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.display.ColorService;
+import org.lineageos.settings.dolby.DolbyUtils;
+import org.lineageos.settings.doze.AodBrightnessService;
+import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.doze.PocketService;
+import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -24,5 +32,33 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
+
+        // DisplayFeature
+        ColorService.startService(context);
+
+        // NFC
+        NfcCameraService.startService(context);
+
+        // Pocket
+        PocketService.startService(context);
+
+        // AOD
+        AodBrightnessService.startService(context);
+
+        // Dirac
+        try {
+            DiracUtils.getInstance(context);
+        } catch (Exception e) {
+            Log.d(TAG, "Dirac is not present in system");
+        }
+
+        // Dolby Atmos
+        // DolbyUtils.getInstance(context).onBootCompleted();
+
+        // Doze
+        // DozeUtils.checkDozeService(context);
+
+        // Refresh Rate
+        RefreshUtils.initialize(context);
     }
 }
