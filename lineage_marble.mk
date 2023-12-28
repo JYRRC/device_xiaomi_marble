5447940db6220a09e362bcce97c6cf4085a66dd8
JYRRC <javixhtsweet@gmail.com>
2023-12-28 18:59:23 +0100

    marble: enable blur
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -17,6 +17,7 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 # Crdroid stuff.
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
 
 # Nuke AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
