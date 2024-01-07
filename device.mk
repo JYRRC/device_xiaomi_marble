da1af2af0cbf8d21185ef0a0a62f25d80d508b63
JYRRC <javixhtsweet@gmail.com>
2024-01-07 16:24:37 +0100

    marble: Build Lineage Health HAL
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- device.mk
+++ device.mk
@@ -326,9 +326,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
-# Lineage Health
-#PRODUCT_PACKAGES += \
-#    vendor.lineage.health-service.default
 
 # Media
 PRODUCT_PACKAGES += \
@@ -428,6 +425,10 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayGlobal \
     SettingsProviderOverlayIndia \
     SettingsProviderOverlayChina
+
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
